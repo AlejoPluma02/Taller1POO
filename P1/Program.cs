0e6ca0b965a8838afca3b1995c48f24b18433564
AlejoPluma02 <fernandezalejo937@gmail.com>
2024-03-19 15:08:17 -0500

    Agregar archivos de proyecto.

---
+++ P1/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace P1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escriba su saldo inicial: ");
+            int saldoInicial = int.Parse(Console.ReadLine());
+            Cuenta objeto1 = new Cuenta(saldoInicial);
+
+            Console.Write("Escriba el monto a acreditar: ");
+            int acreditar = int.Parse(Console.ReadLine());
+            objeto1.Credit(acreditar);
+
+            Console.Write("Escriba el monto a cargar: ");
+            int cargar = int.Parse(Console.ReadLine());
+            objeto1.Cargar(cargar);
+
+            Console.WriteLine("El saldo actual es: " + objeto1.ObtenerSaldo());
+            Console.ReadLine();
+        }
+    }
+
+    class Cuenta
+    {
+        private int saldoActual;
+
+        public Cuenta(int saldoInicial)
+        {
+            if (saldoInicial >= 0)
+            {
+                saldoActual = saldoInicial;
+            }
+            else
+            {
+                saldoInicial = 0;
+                Console.WriteLine("Error: el saldo inicial es inválido");
+            }
+        }
+
+        public void Credit(int acreditar)
+        {
+            saldoActual += acreditar;
+        }
+
+        public void Cargar(int cargar)
+        {
+            if (saldoActual >= cargar)
+            {
+                saldoActual -= cargar;
+            }
+            else
+            {
+                Console.WriteLine("El monto a cargar excede el saldo de la cuenta.");
+            }
+        }
+
+        public int ObtenerSaldo()
+        {
+            return saldoActual;
+        }
+    }
+}
---
+++ P2/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace P2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escriba el código de la pieza: ");
+            String NumeroDePieza = Console.ReadLine();
+
+            Console.Write("Escriba la descripción de la pieza: ");
+            String DescripcionDePieza = Console.ReadLine();
+
+            Console.Write("Escriba la cantidad de piezas: ");
+            int CantidadDeArticulos = int.Parse(Console.ReadLine());
+
+            Console.Write("Escriba el precio unitario de la pieza: ");
+            int PrecioPorArticulo = int.Parse(Console.ReadLine());
+
+            Factura factura = new Factura(NumeroDePieza, DescripcionDePieza, CantidadDeArticulos, PrecioPorArticulo);
+
+            Console.WriteLine("Número de Pieza: " + factura.NumeroDePieza);
+            Console.WriteLine("Descripción de Pieza: " + factura.DescripcionDePieza);
+            Console.WriteLine("Cantidad de Artículos: " + factura.CantidadDeArticulos);
+            Console.WriteLine("Precio por Artículo: " + factura.PrecioPorArticulo);
+            Console.WriteLine("Monto de la Factura: " + factura.ObtenerMontoFactura());
+        }
+    }
+
+    class Factura
+    {
+        public string NumeroDePieza { get; set; }
+        public string DescripcionDePieza { get; set; }
+        public int CantidadDeArticulos { get; set; }
+        public int PrecioPorArticulo { get; set; }
+
+        public Factura(string numeroDePieza, string descripcionDePieza, int cantidadDeArticulos, int precioPorArticulo)
+        {
+            EstablecerNumeroPieza(numeroDePieza);
+            EstablecerDescripcion(descripcionDePieza);
+            EstablecerCantidad(cantidadDeArticulos);
+            EstablecerPrecio(precioPorArticulo);
+        }
+
+        public void EstablecerNumeroPieza(String numeroDePieza)
+        {
+            NumeroDePieza = numeroDePieza;
+        }
+
+        public void EstablecerDescripcion(String descripcionDePieza)
+        {
+            DescripcionDePieza = descripcionDePieza;
+        }
+
+        public void EstablecerCantidad(int cantidadDeArticulos)
+        {
+            CantidadDeArticulos = cantidadDeArticulos > 0 ? cantidadDeArticulos : 0;
+        }
+
+        public void EstablecerPrecio(int precioPorArticulo)
+        {
+            PrecioPorArticulo = precioPorArticulo > 0 ? precioPorArticulo : 0;
+        }
+
+        public int ObtenerMontoFactura()
+        {
+            return CantidadDeArticulos * PrecioPorArticulo;
+        }
+    }
+}
